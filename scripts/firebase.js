0dde7ee44980acd149bb8d2a44bc5f2465408c9a
cjlim1092 <32859465+cjlim1092@users.noreply.github.com>
2020-08-10 00:15:09 -0700

    Create firebase.js

---
+++ scripts/firebase.js
@@ -0,0 +1,15 @@
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBYU3B4VToXrCg9uAEg-YGeouhKOjGEAvo",
+    authDomain: "best-learning-tool-5eb86.firebaseapp.com",
+    databaseURL: "https://best-learning-tool-5eb86.firebaseio.com",
+    projectId: "best-learning-tool-5eb86",
+    storageBucket: "best-learning-tool-5eb86.appspot.com",
+    messagingSenderId: "469466995897",
+    appId: "1:469466995897:web:5e435e5081d6b713464f68",
+    measurementId: "G-HJV4G8X0RH"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
